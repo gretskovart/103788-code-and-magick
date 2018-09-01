17e69f5a0fa51c08643671428e7d0161ce9230a6
Artem <gretskovart@gmail.com>
2018-09-01 19:34:02 +0400

    make a cloud

---
+++ js/stat.js
@@ -0,0 +1,15 @@
+'use strict';
+
+window.renderStatistics = function(ctx, names, times) {
+  ctx.beginPath();
+  ctx.moveTo(0, 75);
+  ctx.bezierCurveTo(0, 75, 50, 0, 100, 50);
+  ctx.bezierCurveTo(100, 50, 150, 0, 200, 50);
+  ctx.bezierCurveTo(200, 50, 250, 0, 300, 75);
+  ctx.bezierCurveTo(300, 75, 250, 150, 200, 100);
+  ctx.bezierCurveTo(200, 100, 150, 150, 100, 100);
+  ctx.bezierCurveTo(100, 100, 50, 150, 0, 75);
+  ctx.stroke();
+
+
+};
