1ed1b29ec96b4c2290c0cb4f830d2e045bce2239
Artem <gretskovart@gmail.com>
2018-09-01 19:52:09 +0400

    add cloud's shadow

--- js/stat.js
+++ js/stat.js
@@ -2,6 +2,19 @@
 
 window.renderStatistics = function(ctx, names, times) {
   ctx.beginPath();
+  ctx.moveTo(10, 85);
+  ctx.bezierCurveTo(10, 85, 60, 10, 110, 60);
+  ctx.bezierCurveTo(110, 60, 160, 10, 210, 60);
+  ctx.bezierCurveTo(210, 60, 260, 10, 310, 85);
+  ctx.bezierCurveTo(310, 85, 260, 160, 210, 110);
+  ctx.bezierCurveTo(210, 110, 160, 160, 110, 110);
+  ctx.bezierCurveTo(110, 110, 60, 160, 10, 85);
+  ctx.stroke();
+
+  ctx.fillStyle = "rgba(0, 0, 0, 0.7)";
+  ctx.fill();
+
+  ctx.beginPath();
   ctx.moveTo(0, 75);
   ctx.bezierCurveTo(0, 75, 50, 0, 100, 50);
   ctx.bezierCurveTo(100, 50, 150, 0, 200, 50);
@@ -13,5 +26,4 @@ window.renderStatistics = function(ctx, names, times) {
 
   ctx.fillStyle = "#fff";
   ctx.fill();
-
 };
