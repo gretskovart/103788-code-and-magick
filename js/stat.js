a7ea6811f9394990f3371b161dbbde3bc76a5981
Artem <gretskovart@gmail.com>
2018-09-01 20:55:20 +0400

    change coordinates of cloud to [100, 10]

--- js/stat.js
+++ js/stat.js
@@ -2,26 +2,26 @@
 
 window.renderStatistics = function(ctx, names, times) {
   ctx.beginPath();
-  ctx.moveTo(10, 145);
-  ctx.bezierCurveTo(10, 145, 80, 10, 150, 60);
-  ctx.bezierCurveTo(150, 60, 220, 10, 290, 60);
-  ctx.bezierCurveTo(290, 60, 360, 10, 430, 145);
-  ctx.bezierCurveTo(430, 145, 360, 280, 290, 230);
-  ctx.bezierCurveTo(290, 230, 220, 280, 150, 230);
-  ctx.bezierCurveTo(150, 230, 80, 280, 10, 145);
+  ctx.moveTo(110, 155);
+  ctx.bezierCurveTo(110, 155, 180, 20, 250, 70);
+  ctx.bezierCurveTo(250, 70, 320, 20, 390, 70);
+  ctx.bezierCurveTo(390, 70, 460, 20, 530, 155);
+  ctx.bezierCurveTo(530, 155, 460, 290, 390, 240);
+  ctx.bezierCurveTo(390, 240, 320, 290, 250, 240);
+  ctx.bezierCurveTo(350, 240, 180, 290, 110, 155);
   ctx.stroke();
 
   ctx.fillStyle = "rgba(0, 0, 0, 0.7)";
   ctx.fill();
 
   ctx.beginPath();
-  ctx.moveTo(0, 135);
-  ctx.bezierCurveTo(0, 135, 70, 0, 140, 50);
-  ctx.bezierCurveTo(140, 50, 210, 0, 280, 50);
-  ctx.bezierCurveTo(280, 50, 350, 0, 420, 135);
-  ctx.bezierCurveTo(420, 135, 350, 270, 280, 220);
-  ctx.bezierCurveTo(280, 220, 210, 270, 140, 220);
-  ctx.bezierCurveTo(140, 220, 70, 270, 0, 135);
+  ctx.moveTo(100, 145);
+  ctx.bezierCurveTo(100, 145, 170, 10, 240, 60);
+  ctx.bezierCurveTo(240, 60, 310, 10, 380, 60);
+  ctx.bezierCurveTo(380, 60, 450, 10, 520, 145);
+  ctx.bezierCurveTo(520, 145, 450, 280, 380, 230);
+  ctx.bezierCurveTo(380, 230, 310, 280, 240, 230);
+  ctx.bezierCurveTo(240, 230, 170, 280, 100, 145);
   ctx.stroke();
 
   ctx.fillStyle = "#fff";
