d3f2569e9254d85a12d58966142b055978964338
Artem <gretskovart@gmail.com>
2018-09-02 15:49:52 +0400

    add bar's with score

--- js/stat.js
+++ js/stat.js
@@ -1,5 +1,12 @@
 'use strict';
 
+var BAR_HEIGHT = 150;
+var BAR_WIDTH = 40;
+var GAP = 50;
+var YOUR_COLOR = 'rgba(255, 0, 0, 1)';
+var saturate = Math.ceil(Math.random() * 100) + '%';
+var othersColor = 'hsl(240,' + saturate + ',50%)';
+
 var renderCloud = function(ctx, x, y, color) {
   ctx.beginPath();
   ctx.moveTo(10 + x, 145 + y);
@@ -15,12 +22,24 @@ var renderCloud = function(ctx, x, y, color) {
   ctx.fill();
 };
 
+var addScore = function(ctx, name, color, x, y) {
+  ctx.fillStyle = '#000';
+  ctx.fillText(name, x, y);
+
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y - 15, BAR_WIDTH, '-' + BAR_HEIGHT);
+};
+
 window.renderStatistics = function(ctx, names, times) {
   renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.3)');
   renderCloud(ctx, 100, 10, '#fff');
 
   ctx.fillStyle = '#000';
-  ctx.font = "16px PT Mono";
-  ctx.fillText("Ура вы победили!", 110 + 135, 90);
-  ctx.fillText("Список результатов:", 110 + 135, 110);
+  ctx.font = '16px PT Mono';
+  ctx.fillText('Ура вы победили!', 235, 60);
+  ctx.fillText('Список результатов:', 235, 70);
+
+  addScore(ctx, 'Вы', YOUR_COLOR, 250, 245);
+  addScore(ctx, 'Иван', othersColor, 250 + GAP, 245);
+  addScore(ctx, 'Юлия', othersColor, 250 + GAP + GAP, 245);
 };
