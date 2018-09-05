1bf768772e26d30a75474b4df91d5906ca06bb71
Artem <gretskovart@gmail.com>
2018-09-05 21:52:04 +0400

    add getWizardName function

--- js/setup.js
+++ js/setup.js
@@ -7,3 +7,39 @@ var showSetup = function () {
 };
 
 showSetup();
+
+var createWizardsArray = function () {
+  var wizardsArray = [];
+  var wizardsQuality = 4;
+
+  for (var i = 0; i < wizardsQuality; i++) {
+    wizardsArray[i] = {
+      name: getWizardName(true),
+      coatColor: wizardsCoatColor,
+      eyesColor: wizardsEyesColor
+    }
+  }
+};
+
+var getWizardName = function (mixName) {
+  var wizardsNames = [
+    [
+      'Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита',
+      'Вашингтон'
+    ], ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая',
+      'Нионго', 'Ирвинг']
+  ];
+  var random = Math.floor(Math.random());
+
+  var getRandomWizardName = function (arr) {
+    return random * arr.length;
+  };
+
+  if (mixName) {
+    return getRandomWizardName(wizardsNames[random]) + ' '
+    + getRandomWizardName(wizardsNames[random]);
+  } else {
+    return getRandomWizardName(wizardsNames[0]) + ' '
+    + getRandomWizardName(wizardsNames[1]);
+  }
+};
